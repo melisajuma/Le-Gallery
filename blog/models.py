40f7421384e7f24e8b2f095cadb32219038fc41b
melisajuma <Melisaakinyi95@gmail.com>
2019-08-26 11:20:36 +0300

    adding the category  model

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,12 @@ class Location(models.Model):
     def save_location(self):
         self.save()
        
+class Category(models.Model):
+    category_name = models.CharField(max_length=40,unique=True)
+   
+def __str__(self):
+    return self.category_name
+   
+def save_category(self):
+    self.save()
+       
