b7549de4c206addd3c867df3f35983f27a8f432b
melisajuma <Melisaakinyi95@gmail.com>
2019-08-26 11:34:07 +0300

    adding url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-   
+    url('^$',views.welcome,name = 'welcome'),
+    url('^about$',views.about,name = 'about'),
+    url('^search/', views.search_results, name='search_results'),
+    url('^image/(\d+)',views.image,name ='image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
