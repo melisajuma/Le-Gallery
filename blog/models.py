b85fee6bb05cbcf91d3112e9025895b72c259730
melisajuma <Melisaakinyi95@gmail.com>
2019-08-26 11:19:48 +0300

    adding the location model

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,14 @@ import datetime as dt
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# Create your models here.
+
+class Location(models.Model):
+    location_name=models.CharField(max_length=30,unique=True)
+    
+    def __str__(self):
+        return self.location_name
+   
+    def save_location(self):
+        self.save()
+       
