bc73344b67a6028600cba76578e6dbef9c6b92a1
melisajuma <Melisaakinyi95@gmail.com>
2019-08-26 11:31:55 +0300

    registering models

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,4 @@ from .models import Image,Location,Category
 # Register your models here.
 class ImageAdmin(admin.ModelAdmin):
     filter_horizontal =('category',)
-    
-admin.site.register(Image)
-admin.site.register(Location)
-admin.site.register(Category)
+  
