6ef0d65e764d34d54fc5dcae8ee89f51b31a544f
melisajuma <Melisaakinyi95@gmail.com>
2019-08-26 11:27:57 +0300

    adding a function for filtering specifications

--- blog/models.py
+++ blog/models.py
@@ -62,6 +62,14 @@ def update_image(cls,current_value,new_value):
     fetched_object = Image.objects.filter(image_name=current_value).update(image_name=new_value)
     return fetched_object
    
+@classmethod
+def filter_by_location(cls,location):
+    filtered_result = cls.objects.filter(image_location__location_name__icontains=location)
+    return filtered_result
+   
+
+    
+    
 
 
    
